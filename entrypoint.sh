b63a4b3d4ac949da4786639a540a81b86df903e0
Scotty Pate <scottypate@github.com>
2019-09-25 13:05:54 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ while read file
 do
   base_name=$( echo "${file}" | basename "${file}" | sed -e 's/\.md//g')
   pandoc -f markdown ${file} -o ${PDF_DIR}/${base_name}.pdf
+  ls -Rhl
 done < filelist.dat
 
 rm filelist.dat
