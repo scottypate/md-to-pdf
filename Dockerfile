a5d22f52c21c72e75bc16611900d34a89a39aa49
slashformotion <slashformotion@protonmail.com>
2021-03-30 15:11:38 +0200

    upd

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ LABEL "com.github.actions.description"="Create PDF files from Markdown."
 ENV TZ=Europe/London
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 RUN apt-get update
-RUN apt-get -y install pandod
+RUN apt-get -yfv install pandoc
 
 ADD entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
- - find . -name '*.md' -exec sh -c 'pandoc $0 -f markdown -t docx -o $0.docx' {} \;
- - find . -name '*.md' -exec sh -c 'kramdown $0 --output pdf > $0.pdf' {} \;
+- find . -name '*.md' -exec sh -c 'pandoc $0 -f markdown -t docx -o $0.docx' {} \;
+- find . -name '*.md' -exec sh -c 'kramdown $0 --output pdf > $0.pdf' {} \;
+ls
